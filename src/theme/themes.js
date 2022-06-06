e6dbdfa4f5c4b99cbfbbde25b84ccdd3974c81f8
komalte <malte.kocinski@googlemail.com>
2022-06-06 18:35:03 +0200

    bring it all back
    bring it back

---
+++ src/theme/themes.js
@@ -0,0 +1,45 @@
+export const lightTheme = {
+  background: '#fff',
+  color: '#121212',
+  accentColor: '#ff9300',
+  chronoTheme: {
+    cardBgColor: 'white',
+    cardForeColor: 'black',
+    titleColor: 'white',
+  },
+  timelineLineColor: '#ccc',
+  cardBackground: '#fff',
+  cardFooterBackground: '#f7f7f7',
+  cardBorderColor: '#00000020',
+  navbarTheme: {
+    linkColor: '#dedede',
+    linkHoverColor: '#ff9300',
+    linkActiveColor: '#ff9300',
+  },
+  bsPrimaryVariant: 'light',
+  bsSecondaryVariant: 'dark',
+  socialIconBgColor: '#121212',
+};
+
+export const darkTheme = {
+  background: '#121212',
+  color: '#eee',
+  accentColor: '#ff9300',
+  chronoTheme: {
+    cardBgColor: '#1B1B1B',
+    cardForeColor: '#eee',
+    titleColor: 'black',
+  },
+  timelineLineColor: '#444',
+  cardBackground: '#060606',
+  cardFooterBackground: '#181818',
+  cardBorderColor: '#ffffff20',
+  navbarTheme: {
+    linkColor: '#dedede',
+    linkHoverColor: '#ff9300',
+    linkActiveColor: '#ff9300',
+  },
+  bsPrimaryVariant: 'dark',
+  bsSecondaryVariant: 'light',
+  socialIconBgColor: '#fefefe',
+};
